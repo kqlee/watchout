e9d7a293c74041522d77a27c13bfc362a8fcde0d
Kent <kentqlee@gmail.com>
2016-05-01 15:08:13 -0700

    Added functionality

--- client/watchout.js
+++ client/watchout.js
@@ -1,136 +1,102 @@
-// // start slingin' some d3 here.
-// $(document).ready(function() {
+var currentScore = 0;
+var bestScore = 0;
+var collisions = 0;
+var numEnemies = 20;
+var width = 800;
+var height = 500;
 
-// //   //Create scoring counters
-// //   var score = 0;
-// //   var bestScore = 0;
+//Board
 
-// //   //Create a new bird, and pass in starting positions for x and y coordinates
-// //   var flappyBird = new Bird(20, 10);
+var canvas = d3.select('body')
+            .append('svg')
+            .attr({'width': width, 'height': height});
 
-// //   //Spacebar
-// //   var moveSelection = function(e) {
-// //     if (e.keyCode === 32) {
-// //       //add pixels to the character to the y axis
-// //       this.y += 10;
-// //     }
-// //   };
+//Player
 
-// //   //Collision Detection
-// //   var checkCollision = function() {
+var drag = d3.behavior.drag()
+             .on('drag', function() {
+               player.attr({
+                 'cx': Math.max(25, Math.min(width - 25, d3.event.x)),
+                 'cy': Math.max(25, Math.min(height - 25, d3.event.y))
+               });
+             });
 
-// //   };
+var player = canvas.selectAll('#player')
+                  .data([{x: width / 2, y: height / 2}])
+                  .enter()
+                  .append('circle')
+                  .attr({
+                    'cx': function(d) {
+                      return d.x;
+                    },
+                    'cy': function(d) {
+                      return d.y;
+                    },
+                    'r': 25,
+                    'id': 'player',
+                    'fill': 'red'
+                  })
+                  .call(drag);
 
-//   var dataset = [10,20,89,40,20,10,44,33,22,66,55];
+//Scores
 
-//   setInterval(function() {
-//     var index = Math.floor(Math.random()*10);
-//     var newHeight = Math.floor(Math.random()*200)+50;
-//     dataset.splice(index, 1, newHeight);
+var updateScores = function() {
+  d3.select('.current').select('span').text(currentScore++);
+  if (currentScore > bestScore) {
+    bestScore = currentScore;
+    d3.select('.highscore').select('span').text(bestScore - 1);
+  }
+};
 
-//      var barPadding = 20;
+//Enemies
+var enemies = [];
 
-//   var tube = board.selectAll("rect")
-//    .data(dataset)
-//    .enter()
-//    .append("rect")
-//    .attr("x", function(d, i) {
-//      return i * (w / dataset.length);
-//    }).attr("y", function(d) {
-//      return h - (d * 4);
-//    }).attr("width", w / dataset.length - barPadding)
-//    .attr("height", function(d) {
-//      return d * 4;
-//    })
-//     .attr("fill", "green");
+for (var i = 0; i < numEnemies; i++) {
+  enemies.push({
+    id: i,
+    x: Math.random() * 100,
+    y: Math.random() * 100
+  });
+}
 
-//   }, 3000);
+var createEnemies = canvas.selectAll('circle')
+                          .data(enemies)
+                          .enter()
+                          .append('circle')
+                          .attr({
+                            'cx': function() {
+                              return width - (Math.random() * width) - 50;
+                            },
+                            'cy': function() {
+                              return height - (Math.random() * height) - 50;
+                            },
+                            'r': 15,
+                            'class': 'enemies',
+                            'fill': 'blue'
+                          });
 
+var move = function() {
+  canvas.selectAll('.enemies')
+        .transition()
+        .ease('linear')
+        .duration(2000)
+        .attr({
+          'cx': function() {
+            return (Math.random() * width);
+          },
+          'cy': function() {
+            return (Math.random() * height);
+          },
+          'r': (Math.random() * 25) + 5
+        });
+};
 
-// //   //event listener when the document is ready
-// //   (function docReady() {
-// //     window.addEventListener('keydown', moveSelection);
-// //   })();
-// });
-
-
-
-// //Game board
-// var h = 500;
-// var w = 800;
-
-// var board = d3.select("body")
-//               .append("svg")
-//               .attr("id", "board")
-//               .attr("height", h)
-//               .attr("width", w)
-//               .style("background-color", "lightblue");
-
-// var flappyBird = board.append("rect")
-//                       .attr("id", "flappy")
-//                       .attr("x", 25)  //sets the padding on the left
-//                       .attr("y", 150) //sets the padding from the top
-//                       .attr("width", 50)  //sets the width size
-//                       .attr("height", 50) //sets the height
-//                       .attr("fill", "yellow");
-
-
-
-// // Create tubes as obstacles periodically    
-// // var tubeList = [10, 20, 30, 40, 50];
-
-// // setInterval(function() {
-// //   tubes.push(Math.random() * 20);
-// // }, 2000);
-
-
-// var tubes = board.append("div")
-//                 .data(tubeList)
-//                 .enter()
-//                 .attr("class", "tube");
-
-//   var dataset = [10,20,89,40,20,10,44,33,22,66,55];
-
-//   setInterval(function() {
-//     var index = Math.floor(Math.random()*10);
-//     var newHeight = Math.floor(Math.random()*200)+50;
-//     dataset.splice(index, 1, newHeight);
-
-//      var barPadding = 20;
-
-//   var tube = board.selectAll("rect")
-//    .data(dataset)
-//    .enter()
-//    .append("rect")
-//    .attr("x", function(d, i) {
-//      return i * (w / dataset.length);
-//    }).attr("y", function(d) {
-//      return h - (d * 4);
-//    }).attr("width", w / dataset.length - barPadding)
-//    .attr("height", function(d) {
-//      return d * 4;
-//    })
-//     .attr("fill", "green");
-
-//   }, 3000);
-
-//   var barPadding = 20;
-
-//   var tube = board.selectAll("rect")
-//    .data(dataset)
-//    .enter()
-//    .append("rect")
-//    .attr("x", function(d, i) {
-//      return i * (w / dataset.length);
-//    }).attr("y", function(d) {
-//      return h - (d * 4);
-//    }).attr("width", w / dataset.length - barPadding)
-//    .attr("height", function(d) {
-//      return d * 4;
-//    })
-//     .attr("fill", "green");
-
-    
+//Collisions
 
 
 
+
+//Invocation of functions
+
+setInterval(updateScores, 1000);
+setInterval(move, 2000);
